f691a40204005bad44b67c796a410fc0493d1ab5
TZZ <1789803837@qq.com>
2020-07-12 02:49:36 +0800

    完成菜单修复

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+
+<!--    <v-bottom-navigation-->
+<!--      :value="activeBtn"-->
+<!--      grow-->
+<!--      color="teal"-->
+<!--      class="bottom-menu"-->
+<!--      width="60%"-->
+<!--    >-->
+<!--      <v-btn>-->
+<!--        <span>Blog</span>-->
+<!--        <v-icon>mdi-post-outline</v-icon>-->
+<!--      </v-btn>-->
+
+<!--      <v-btn>-->
+<!--        <span>Favorites</span>-->
+<!--        <v-icon>mdi-heart</v-icon>-->
+<!--      </v-btn>-->
+
+<!--      <v-btn>-->
+<!--        <span>Nearby</span>-->
+<!--        <v-icon>mdi-map-marker</v-icon>-->
+<!--      </v-btn>-->
+<!--    </v-bottom-navigation>-->
+    <TMenu></TMenu>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
+import TMenu from './components/TMenu'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    TMenu
+  },
+
+  data: () => ({
+    //
+    activeBtn: 1
+  })
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.bottom-menu {
+  margin-bottom: 30px;
+  margin-left: 20%;
 }
 </style>
---
+++ src/components/TMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-bottom-navigation
+    :value="activeBtn"
+    grow
+    color="teal"
+    width="60%"
+    class="menu"
+  >
+    <v-btn>
+      <span>Blog</span>
+      <v-icon>mdi-post-outline</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeBtn: 1
+    }
+  }
+}
+</script>
+<style scoped>
+.menu {
+  margin-left: 20%;
+  margin-bottom: 30px;
+}
+</style>
